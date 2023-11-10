c822175c034de9da216aa008e6c4ab1c6e01508a
Diego Duarte <duartqx@gmail.com>
2023-11-10 20:37:38 -0300

    updated comments migration

--- infrastructure/repositories/postgres/migrations/up/00004_create_comments_table.sql
+++ infrastructure/repositories/postgres/migrations/up/00004_create_comments_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Comments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    comment_text TEXT,
-    parent_id UUID NOT NULL REFERENCES Comments(id),
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    parent_id UUID REFERENCES Comments(id) DEFAULT NULL,
     creator_id UUID NOT NULL REFERENCES Users(id),
     thread_id UUID NOT NULL REFERENCES Threads(id)
 );
