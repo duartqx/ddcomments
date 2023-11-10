fd056fb62daa642f35df55eeb71f440b85b78627
Diego Duarte <duartqx@gmail.com>
2023-11-10 20:43:09 -0300

    gorilla mux and server

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/duartqx/ddcomments/api/gorilla/router"
 	r "github.com/duartqx/ddcomments/infrastructure/repositories/postgres"
 )
 
@@ -14,6 +20,42 @@ func main() {
 	}
 	defer db.Close()
 
-	log.Println("Hello World!")
+	mux := router.GetMux(db)
 
+	port := ":8000"
+
+	srv := &http.Server{
+		Handler:      mux,
+		Addr:         "127.0.0.1" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("| Listening at port " + port)
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("| Shutting down")
+	os.Exit(0)
 }
