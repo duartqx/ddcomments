24cfc614ec5f66d119dbea2a14f960a13c95fb4a
Diego Duarte <duartqx@gmail.com>
2023-11-11 15:25:47 -0300

    Using HttpResponse on controller

--- api/gorilla/controllers/comment/CommentController.go
+++ api/gorilla/controllers/comment/CommentController.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 
+	h "github.com/duartqx/ddcomments/application/http"
 	cs "github.com/duartqx/ddcomments/application/services/comment"
 )
 
@@ -21,34 +22,46 @@ func GetNewCommentController(commentService *cs.CommentService) *CommentControll
 }
 
 func (cc CommentController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	var response *h.HttpResponse
+
 	switch r.Method {
 	case http.MethodGet:
-		cc.get(w, r)
+		response = cc.get(r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if body, err := json.Marshal(response.Body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(response.Status)
+		w.Write(body)
 	}
 }
-func (cc CommentController) get(w http.ResponseWriter, r *http.Request) {
+
+func (cc CommentController) get(r *http.Request) *h.HttpResponse {
 
 	thread_id, err := uuid.Parse(mux.Vars(r)["thread_id"])
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+		return &h.HttpResponse{
+			Status: http.StatusBadRequest, Body: "Bad Request",
+		}
 	}
 
 	comments, err := cc.commentService.GetAllCommentsByThreadId(thread_id)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return &h.HttpResponse{
+			Status: http.StatusInternalServerError, Body: "Internal Server Error",
+		}
 	}
 
 	results := map[string]interface{}{
 		"results": comments,
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(results)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	return &h.HttpResponse{Status: http.StatusOK, Body: results}
 }
