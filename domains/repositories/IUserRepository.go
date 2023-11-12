a7cbc0adaff862753ea526a16462c6c820d10448
Diego Duarte <duartqx@gmail.com>
2023-11-12 18:29:48 -0300

    IUserRepository updates

--- domains/repositories/IUserRepository.go
+++ domains/repositories/IUserRepository.go
@@ -9,4 +9,10 @@ import (
 type IUserRepository interface {
 	FindByID(id uuid.UUID) (m.User, error)
 	FindByEmail(email string) (m.User, error)
+	Update(user m.User) error
+	All() (*[]m.User, error)
+	Create(user m.User) error
+	Delete(user m.User) error
+	ExistsById(id uuid.UUID) *bool
+	ExistsByEmail(email string) *bool
 }
