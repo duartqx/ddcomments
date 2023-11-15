0b076c965b88bfb4038392e1d771af8d315fbb32
Diego Duarte <duartqx@gmail.com>
2023-11-15 01:08:19 -0300

    migrations update

--- infrastructure/repositories/postgres/migrations/up/00003_create_thread_table.sql
+++ infrastructure/repositories/postgres/migrations/up/00003_create_thread_table.sql
@@ -2,5 +2,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS Threads (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    slug TEXT NOT NULL
+    slug TEXT NOT NULL,
+    creator_id UUID NOT NULL REFERENCES Users(id) ON DELETE CASCADE
 );
--- infrastructure/repositories/postgres/migrations/up/00004_create_comments_table.sql
+++ infrastructure/repositories/postgres/migrations/up/00004_create_comments_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Comments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     comment_text TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    parent_id UUID REFERENCES Comments(id) DEFAULT NULL,
-    creator_id UUID NOT NULL REFERENCES Users(id),
-    thread_id UUID NOT NULL REFERENCES Threads(id)
+    parent_id UUID DEFAULT NULL REFERENCES Comments(id) ON DELETE SET NULL,
+    creator_id UUID NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+    thread_id UUID NOT NULL REFERENCES Threads(id) ON DELETE CASCADE
 );
