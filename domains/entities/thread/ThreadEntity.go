1b4a58d7e15b3aea1ee532d866b7f8df399b8422
Diego Duarte <duartqx@gmail.com>
2023-11-15 01:11:37 -0300

    Thread interface expansion

--- domains/entities/thread/ThreadEntity.go
+++ domains/entities/thread/ThreadEntity.go
@@ -7,11 +7,47 @@ import (
 )
 
 type ThreadEntity struct {
-	Id       uuid.UUID    `db:"id" json:"id"`
-	Slug     string       `db:"slug" json:"slug"`
-	Comments *[]m.Comment `json:"comments"`
+	Id        uuid.UUID    `db:"id" json:"id"`
+	CreatorId uuid.UUID    `db:"creator_id" json:"-"`
+	Slug      string       `db:"slug" json:"slug"`
+	Comments  *[]m.Comment `json:"comments"`
+
+	Creator m.User `json:"creator"`
 }
 
 func (t ThreadEntity) GetId() uuid.UUID {
 	return t.Id
+}
+
+func (t ThreadEntity) GetCreatorId() uuid.UUID {
+	return t.CreatorId
+}
+
+func (t ThreadEntity) GetCreator() m.User {
+	return t.Creator
+}
+
+func (t ThreadEntity) GetSlug() string {
+	return t.Slug
+}
+
+func (t *ThreadEntity) SetId(id uuid.UUID) m.Thread {
+	t.Id = id
+	return t
+}
+
+func (t *ThreadEntity) SetCreatorId(id uuid.UUID) m.Thread {
+	t.CreatorId = id
+	return t
+}
+
+func (t *ThreadEntity) SetCreator(creator m.User) m.Thread {
+	t.CreatorId = creator.GetId()
+	t.Creator = creator
+	return t
+}
+
+func (t *ThreadEntity) SetSlug(slug string) m.Thread {
+	t.Slug = slug
+	return t
 }
--- domains/models/Thread.go
+++ domains/models/Thread.go
@@ -4,4 +4,12 @@ import "github.com/google/uuid"
 
 type Thread interface {
 	GetId() uuid.UUID
+	GetCreator() User
+	GetCreatorId() uuid.UUID
+	GetSlug() string
+
+	SetId(id uuid.UUID) Thread
+	SetCreator(User) Thread
+	SetCreatorId(id uuid.UUID) Thread
+	SetSlug(slug string) Thread
 }
